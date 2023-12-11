4b4e57006ab80da9871385e844a9059f1b3fdce3
IsmaelNjama <petermailsnjama@gmail.com>
2023-12-11 23:46:51 +0200

    global added midleware

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
-require('dotenv').config(); 
-require ('./utils/postgres.js').run();
+require("dotenv").config();
+require("./utils/postgres.js").run();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const PORT = process.env.PORT || 2555;
-
 
 app.use(express.json());
 
-app.use(cors({ 
-    origin: '*'
-  }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-  
-app.use('/users', require ('./routes/users.routes.js'));
-app.use('/auth', require('./routes/auth.routes.js'));
+//middleware
 
+app.use((req, res, next) => {
+  console.log("middleware working okay");
+
+  next();
+});
+
+//routes
+
+app.use("/users", require("./routes/users.routes.js"));
+app.use("/auth", require("./routes/auth.routes.js"));
 
 app.use((err, req, res, next) => {
   console.log(err);
 
   try {
-      const [statusCode, msg] = err;
+    const [statusCode, msg] = err;
 
-      res.status(statusCode).send({
-          error: true,
-          message: msg
-      })
+    res.status(statusCode).send({
+      error: true,
+      message: msg,
+    });
   } catch (error) {
-      res.status(500).send({
-          error: true,
-          message: err.message
-      })
+    res.status(500).send({
+      error: true,
+      message: err.message,
+    });
   }
-})
-
+});
 
 app.listen(2555, function () {
-    console.log(`listening on localhost ${PORT}`)
-  })
+  console.log(`listening on localhost ${PORT}`);
+});
