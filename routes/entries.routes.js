67f8c513935b9bb971734f91166255399fe60465
Xin <78652542+him-li@users.noreply.github.com>
2023-12-18 14:14:44 +0200

    adjustment for frontend

--- routes/entries.routes.js
+++ routes/entries.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const entriesController = require('../controllers/entries.controller');
 
-
 // router.post('/', validate(postUserSchema), usersController.addUser);
 router.post('/', entriesController.addEntry);
 router.get('/', entriesController.getEntries);
@@ -11,5 +10,4 @@ router.get('/:entryId', entriesController.getEntryById);
 router.put('/:entryId', entriesController.updateEntry);
 router.delete('/:entryId', entriesController.deleteEntry);
 
-
-module.exports = router; 
+module.exports = router;
--- services/entries.services.js
+++ services/entries.services.js
@@ -1,109 +1,109 @@
-const query = require("../utils/postgres").query;
+const query = require('../utils/postgres').query;
 
 const services = {
-  addEntry: async ({
-    title,
-    isgivinghelp,
-    category,
-    availability,
-    description,
-    country,
-    town,
-    urgent,
-  }) => {
-    try {
-      const resp = await query(
-        `
+	addEntry: async ({
+		title,
+		isgivinghelp,
+		category,
+		availability,
+		description,
+		country,
+		region,
+		isurgent,
+	}) => {
+		try {
+			const resp = await query(
+				`
                 INSERT INTO entries
-                (title, isgivinghelp, category, availability, description, country, town, urgent)
+                (title, isgivinghelp, category, availability, description, country, region, isurgent)
                 VALUES
-                ($1, $2, $3, $4, $5, $6, $7, $8)
+                ($1, $2, $3, $4, $5, $6, $7, $8, $9)
                 RETURNING id;
             `,
-        [
-          title,
-          isgivinghelp,
-          category,
-          availability,
-          description,
-          country,
-          town,
-          urgent,
-        ]
-      );
+				[
+					title,
+					isgivinghelp,
+					category,
+					availability,
+					description,
+					country,
+					region,
+					isurgent,
+				]
+			);
 
-      return resp.rows[0].id;
-    } catch (error) {
-      console.error(error);
-    }
-  },
+			return resp.rows[0].id;
+		} catch (error) {
+			console.error(error);
+		}
+	},
 
-  getAllEntries: async () => {
-    const data = await query(`
+	getAllEntries: async () => {
+		const data = await query(`
             SELECT * 
             FROM entries 
             ORDER BY id
         `);
-    return data.rows;
-  },
+		return data.rows;
+	},
 
-  getEntryById: async (entryId) => {
-    const entry = await query(
-      `
+	getEntryById: async (entryId) => {
+		const entry = await query(
+			`
             SELECT * FROM entries
             WHERE id = $1
         `,
-      [entryId]
-    );
-    return entry.rows[0];
-  },
+			[entryId]
+		);
+		return entry.rows[0];
+	},
 
-  getEntriesByQuery: async (
-    q = "",
-    sort = "id",
-    direction = "asc",
-    limit = "10",
-    skip = "0"
-  ) => {
-    const data = await query(`
+	getEntriesByQuery: async (
+		q = '',
+		sort = 'id',
+		direction = 'asc',
+		limit = '10',
+		skip = '0'
+	) => {
+		const data = await query(`
         SELECT *
         FROM entries
         WHERE title ILIKE '%${q}%' 
         or category ILIKE '%${q}%' 
         or availability ILIKE '%${q}%' or description ILIKE '%${q}%' 
         or country ILIKE '%${q}%' 
-        or town ILIKE '%${q}%'  
+        or region ILIKE '%${q}%'  
         ORDER BY ${sort} ${direction}
         LIMIT ${limit}
         OFFSET ${skip};
         `);
 
-    return data.rows;
-  },
+		return data.rows;
+	},
 
-  updateEntry: async (entryId, body) => {
-    await query(
-      `
+	updateEntry: async (entryId, body) => {
+		await query(
+			`
             UPDATE entries 
             SET 
                 ${Object.keys(body)
-                  .map((key) => `${key} = '${body[key]}'`)
-                  .join(", ")}
+									.map((key) => `${key} = '${body[key]}'`)
+									.join(', ')}
             WHERE id = $1
         `,
-      [entryId]
-    );
-  },
+			[entryId]
+		);
+	},
 
-  deleteEntry: async (entryId) => {
-    await query(
-      `
+	deleteEntry: async (entryId) => {
+		await query(
+			`
             DELETE FROM entries 
             WHERE id = $1
         `,
-      [entryId]
-    );
-  },
+			[entryId]
+		);
+	},
 };
 
 module.exports = services;
